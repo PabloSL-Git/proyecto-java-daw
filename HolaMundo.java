1dd4aef6914dad5b590e6b7e33b2a334e5cfb91f
PabloSL-Git <santamarialoredo@gmail.com>
2025-10-16 13:37:04 +0200

    cambioholamundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, Mundo! Este es mi primer programa versionado con Git");
+        System.out.println("La gestión de versiones es una competencia clave");
     }
     
 }
